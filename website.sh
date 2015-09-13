1177d81ae4dd122feb76e2b932df4c9aa0229908
root <root@simonsmh.simonsmh.tk>
2015-09-13 12:46:26 +0800

    update

--- website.sh
+++ website.sh
@@ -1,6 +1,35 @@
-#!/bin/bash
+#!/bin/sh
+echo 1.更新网页
+echo 2.删除链接
+echo 3.上传网页
+echo Please choose:
+read  M
+echo
+if [ "$M" = "1" ]
+then
+echo 选项1
 cd /var/www
 rm -rf /var/www/simonsmh.tk
 git clone https://github.com/simonsmh/simonsmh.tk.git
 cp -Rf /var/wwwfiles/links/. /www/simonsmh.tk
+elif [ "$M" = "2" ]
+then
+echo 选项2
+cd /var/www/simonsmh.tk
+rm -rf /var/www/simonsmh.tk/CAKEY
+rm -rf /var/www/simonsmh.tk/files
+rm -rf /var/www/simonsmh.tk/sfiles
+elif [ "$M" = "3" ]
+then
+echo 选项3
+cd /var/www/simonsmh.tk
+rm -rf /var/www/simonsmh.tk/CAKEY
+rm -rf /var/www/simonsmh.tk/files
+rm -rf /var/www/simonsmh.tk/sfiles
+git add .
+git commit -m 'update'
+git push
+else
+echo 正在退出
+fi
 exit 0
