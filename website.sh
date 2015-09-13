31516a27453407ed93a39a6aa9339103fe148515
Simon Shi <simonsmh@gmail.com>
2015-09-13 19:54:16 +0800

    Update website.sh

--- website.sh
+++ website.sh
@@ -2,9 +2,9 @@
 echo 1.更新网页
 echo 2.删除链接
 echo 3.上传网页
+echo 4.加入链接
 echo Please choose:
 read  M
-echo
 if [ "$M" = "1" ]
 then
 echo 选项1
@@ -15,22 +15,27 @@ cp -Rf /var/wwwfiles/links/. /var/www/simonsmh.tk
 elif [ "$M" = "2" ]
 then
 echo 选项2
-cd /var/www/simonsmh.tk
 rm -rf /var/www/simonsmh.tk/CAKEY
 rm -rf /var/www/simonsmh.tk/files
 rm -rf /var/www/simonsmh.tk/sfiles
 elif [ "$M" = "3" ]
 then
 echo 选项3
-cd /var/www/simonsmh.tk
 rm -rf /var/www/simonsmh.tk/CAKEY
 rm -rf /var/www/simonsmh.tk/files
 rm -rf /var/www/simonsmh.tk/sfiles
+cd /var/www/simonsmh.tk/
 git config credential.helper store
 git add .
 git commit -m 'update'
 git push
+cp -Rf /var/wwwfiles/links/. /var/www/simonsmh.tk
+elif [ "$M" = "4" ]
+then
+echo 选项4
+cp -Rf /var/wwwfiles/links/. /var/www/simonsmh.tk
 else
-echo 正在退出
+echo 不选
 fi
+echo 正在退出
 exit 0
