8a4f87008d5614e9664ad003842773602cbef87e
simonsmh <simonsmh@gmail.com>
2015-10-03 22:00:26 +0800

    update

--- files.php
+++ files.php
@@ -22,7 +22,7 @@
   <nav class="light-blue lighten-1" role="navigation">
     <div class="nav-wrapper container">
       <a id="logo-container" href="files.php" class="brand-logo">
-        文件存储
+        文件储存
       </a>
       <ul class="right hide-on-med-and-down">
         <li>
