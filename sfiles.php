562b9c45a7c3b8496ca04fc6e8acc8bb1d1c655d
simonsmh <simonsmh@gmail.com>
2015-11-04 19:43:34 +0800

    update

---
+++ sfiles.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+<html lang="zh-cn">
+  <?php include( "header.php"); ?>
+    
+    <body>
+      <nav class="light-blue darken-1" role="navigation">
+        <div class="nav-wrapper container">
+          <a id="logo-container" href="files.php" class="brand-logo">
+            私密文件
+          </a>
+          <?php include( "nav.php"); ?>
+            <div class="section no-pad-bot" id="index-banner">
+              <div class="container">
+                <br>
+                <br>
+                <h2 class="header center orange-text">
+                  &nbsp;simonsmh
+                  </br>
+                  私密文件
+                </h2>
+                <div class="row center">
+                  <h5 class="header col s12 light">
+                    绅士，优雅而不污！
+                  </h5>
+                </div>
+                <div class="progress">
+                  <div class="indeterminate blue">
+                  </div>
+                </div>
+                <br>
+              </div>
+            </div>
+            <div class="container">
+              <div class="section">
+                <form id="file" name="file" action="upload_file.php" method="post" enctype="multipart/form-data">
+                  <div class="file-field input-field">
+                    <div class="btn orange">
+                      <span>
+                        File
+                      </span>
+                      <input type="file" name="file" id="file">
+                    </div>
+                    <div class="file-path-wrapper">
+                      <input class="file-path validate" type="text" placeholder="Upload files">
+                    </div>
+                    <input onclick="submit()" type="checkbox" class="filled-in" id="filled-in-box">
+                    <label for="filled-in-box">
+                      确认并上传
+                    </label>
+                  </div>
+                </form>
+                <br>
+                <iframe src="files/" width="100%" height="500" frameborder="0">
+                  <p>
+                    visit http://simonsmh.tk/files/
+                  </p>
+                </iframe>
+              </div>
+              <br>
+              <br>
+            </div>
+            <?php include( "footer.php"); ?>
---
+++ upload_sfile.php
@@ -0,0 +1,32 @@
+<html class="js" lang="zh-CN">
+<head>
+<meta http-equiv="content-type" content="text/html; charset=UTF-8">
+<meta http-equiv=refresh content="5;url=files.php">
+<meta name="viewport" content="width=device-width">
+</head>
+<body><a href="sfiles.php">../</a></br>
+<?php
+  if ($_FILES["file"]["error"] > 0)
+    {
+    echo "Return Code: " . $_FILES["file"]["error"] . "<br />";
+    }
+  else
+    {
+    echo "Upload: " . $_FILES["file"]["name"] . "<br />";
+    echo "Type: " . $_FILES["file"]["type"] . "<br />";
+    echo "Size: " . ($_FILES["file"]["size"] / 1024) . " Kb<br />";
+    echo "Temp file: " . $_FILES["file"]["tmp_name"] . "<br />";
+
+    if (file_exists("sfiles/" . $_FILES["file"]["name"]))
+      {
+      echo $_FILES["file"]["name"] . " already exists. ";
+      }
+    else
+      {
+      move_uploaded_file($_FILES["file"]["tmp_name"],
+      "sfiles/" . $_FILES["file"]["name"]);
+      echo "Stored in: " . sfiles/" . $_FILES["file"]["name"];
+      }
+    }
+?>
+</body>
