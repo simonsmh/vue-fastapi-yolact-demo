3c63dee0f5e19a5fb7c78e404fd333e772a97348
simonsmh <simonsmh@gmail.com>
2015-11-06 20:09:08 +0800

    update

--- add_review.php
+++ add_review.php
@@ -6,6 +6,20 @@ $time = date('Y-m-d H:i',time());
 if (($user=="") and ($content=="")){
 echo "<script type='text/javascript'>alert('请填写内容');</script>";
 }else{
+$user = str_replace('<',"&lt;",$user);
+$user = str_replace('>',"&gt;",$user);
+$user = trim($user);
+$user = preg_replace("/\t/","",$user);
+$user = preg_replace("/\r\n/","<br/>",$user);
+$user = preg_replace("/\r/","<br/>",$user);
+$user = preg_replace("/\n/","<br/>",$user);
+$content = str_replace('<',"&lt;",$content);
+$content = str_replace('>',"&gt;",$content);
+$content = trim($content);
+$content = preg_replace("/\t/","",$content);
+$content = preg_replace("/\r\n/","<br/>",$content);
+$content = preg_replace("/\r/","<br/>",$content);
+$content = preg_replace("/\n/","<br/>",$content);
 $txt = fopen('review.txt','a+');
 $hh = "\r\n";
 $zc = "用户: $user 时间： $time <br> 留言： $content <br> $hh ";
