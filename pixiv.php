309e190a169556e0e3ed5b51d97fbb52a8ef1e79
simonsmh <simonsmh@gmail.com>
2016-09-27 15:36:35 +0800

    update

--- pixiv.php
+++ pixiv.php
@@ -10,11 +10,20 @@ $data = curl_exec($ch);
 curl_close($ch);
 return $data;
 }
-//图片url指向此文件即可,参数?num=NUM为排行榜第NUM-1位，限制50
+//图片url指向此文件即可，参数?num=NUM为排行榜第NUM+1位，限制49，不指定为随机数
+if (isset($_GET['num'])) {
 $i = max(0,$_GET["num"]);
+} else {
+$i = mt_rand(0,49);
+}
 //设置图片头
 header('Content-type: image/jpg');
-$html = curl_get('http://www.pixiv.net/ranking.php?mode=daily&content=illust');
+//设置默认模式，参数?mode=为排行榜类型
+$m = "daily";
+if (isset($_GET['mode'])) {
+$m = $_GET['mode'];
+}
+$html = curl_get('http://www.pixiv.net/ranking.php?mode='.$m.'&content=illust');
 //匹配缩略图url \d为整数数字，{num}为位数
 preg_match_all('|http://i\d\.pixiv\.net/c/240x480/img-master/img/\d{4}/\d{2}/\d{2}/\d{2}/\d{2}/\d{2}/(.*?\.\w{3})|', $html, $image);
 //拼凑高分辨率图片url
@@ -23,8 +32,18 @@ $url = str_ireplace('_master1200','',$url);
 $image[1][$i] = str_ireplace('_p0_master1200.jpg','',$image[1][$i]);
 //输出图片
 $f = curl_get($url, array('User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/55.0.2868.3 Safari/537.36','Referer: http://www.pixiv.net/member_illust.php?mode=medium&illust_id='.$image[1][$i]));
+//尝试jpg
 if ( preg_match("/404/i", $f ) ) {
 $url = str_ireplace('jpg','png',$url);
+$f = curl_get($url, array('User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/55.0.2868.3 Safari/537.36','Referer: http://www.pixiv.net/member_illust.php?mode=medium&illust_id='.$image[1][$i]));
+}
+//尝试小图
+if ( preg_match("/404/i", $f ) ) {
+$url = str_ireplace('240x480','1200x1200',$image[0][$i]);
+$f = curl_get($url, array('User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/55.0.2868.3 Safari/537.36','Referer: http://www.pixiv.net/member_illust.php?mode=medium&illust_id='.$image[1][$i]));
+}
+if ( preg_match("/404/i", $f ) ) {
+$url = str_ireplace('240x480','600x600',$image[0][$i]);
 $f = curl_get($url, array('User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/55.0.2868.3 Safari/537.36','Referer: http://www.pixiv.net/member_illust.php?mode=medium&illust_id='.$image[1][$i]));
 }
 echo $f;
