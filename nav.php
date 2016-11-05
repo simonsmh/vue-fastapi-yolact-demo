7f756ffbcff4cc0801aac469887ecf840cd6a178
simonsmh <simonsmh@gmail.com>
2016-11-05 18:40:01 +0800

    2

---
+++ nav.php
@@ -0,0 +1,50 @@
+  <li>
+    <a href="index.php" class="waves-effect black-text">
+      主页
+    </a>
+  </li>
+  <li>
+    <a href="review.php" class="waves-effect black-text">
+      留言板
+    </a>
+  </li>
+  <li>
+    <a href="files.php" class="waves-effect black-text">
+      文件储存
+    </a>
+  </li>
+  <li>
+    <a href="source.php" class="waves-effect black-text">
+      杂货铺
+    </a>
+  </li>
+  <li>
+    <a href="ty.php" class="waves-effect black-text">
+      台风监测
+    </a>
+  </li>
+  <li>
+    <a href="shell.php" class="waves-effect black-text">
+      Shell
+    </a>
+  </li>
+  <li>
+    <a href="https://bt.simonsmh.cc/" class="waves-effect black-text">
+      离线下载
+    </a>
+  </li>
+  <li>
+    <a href="https://tieba.simonsmh.cc/" class="waves-effect black-text">
+      贴吧云签
+    </a>
+  </li>
+  <li>
+    <a href="https://blog.simonsmh.cc/" class="waves-effect black-text">
+      后花园
+    </a>
+  </li>
+  <li>
+    <a href="glype/" class="waves-effect black-text">
+      Glype
+    </a>
+  </li>
