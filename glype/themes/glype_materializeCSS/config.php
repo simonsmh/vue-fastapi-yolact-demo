4c7cf16af44a0a612ec12e09f33a6104d83dc9d0
simonsmh <simonsmh@gmail.com>
2016-11-05 17:43:41 +0800

    dev

---
+++ glype/themes/glype_materializeCSS/config.php
@@ -0,0 +1,55 @@
+<?php
+/*******************************************************************
+* Glype Proxy Script
+* 
+* This theme configuration file allows easy customization of the
+* theme without editing the HTML templates.
+*******************************************************************
+* Theme: Simple
+* Author: Glype
+* Website: http://www.glype.com/
+******************************************************************/
+
+/*****************************************************************
+* Themes can use "theme replacements". These are HTML tags of the format
+* <!--[tag_name]--> in the template files. To automatically replace
+* these placeholders with other text, use the $themeReplace array.
+*  e.g.
+* <!--[tag_name]--> will be replaced with the value of $themeReplace['tag_name']
+******************************************************************/
+
+// Website title
+$themeReplace['title'] = <<<OUT
+simonsmh - server Website
+OUT;
+
+// Website home
+$themeReplace['home'] = <<<OUT
+https://app.simonsmh.cc/
+OUT;
+
+// Page title
+$themeReplace['header'] = <<<OUT
+simonsmh<br>Glype
+OUT;
+
+// Descibtion
+$themeReplace['describe'] = <<<OUT
+科学上网
+OUT;
+
+// Who are you
+$themeReplace['who'] = <<<OUT
+simonsmh
+OUT;
+
+// CDN (please update it frequently)
+$themeReplace['cdn'] = <<<OUT
+https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.8
+OUT;
+
+//Jquery CDN (please update it frequently)
+$themeReplace['jquery'] = <<<OUT
+https://cdnjs.cloudflare.com/ajax/libs/jquery/2.2.0
+OUT;
+
