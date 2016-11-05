67d6e5f0f4663f8f1ad49e70aa074352a0557d89
simonsmh <simonsmh@gmail.com>
2016-11-05 18:32:46 +0800

    1

---
+++ header.php
@@ -0,0 +1,14 @@
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, minimum-scale=1, user-scalable=no" />
+<meta name="theme-color" content="#3f51b5">
+<style type="text/css">body{font-family:"思源黑体 CN Regular","思源黑体 Regular","Noto Sans S Chinese","Roboto","微软雅黑","Microsoft YaHei",sans-serif;}.indigo{opacity:.92;}</style>
+<link rel="shortcut icon" type="image/x-icon" href="favicon.ico" media="screen" />
+<link href="css/googleapi.css" type="text/css" rel="stylesheet" media="screen,projection" />
+<link href="css/materialize.css" type="text/css" rel="stylesheet" media="screen,projection" />
+<title>
+<?php 
+echo $title;
+?>
+</title>
+</head>
