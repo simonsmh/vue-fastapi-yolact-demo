a42b1170160e98082c69ccd1269736f40678b654
simonsmh <simonsmh@gmail.com>
2017-02-04 16:48:29 +0800

    dist update

--- source.php
+++ source.php
@@ -17,14 +17,13 @@ $handle = popen('cd '.$dir.' && git pull && chmod -R 777 * && chown -R www-data 
 $read = stream_get_contents($handle);
 printf($read);
 pclose($handle);
-}
-/*elseif($_GET["bloghook"] == 1){
+}elseif($_GET["bloghook"] == 1){
 $dir = '/var/www/simonsmh/';
-$handle = popen('cd '.$dir.' && git clone https://github.com/simonsmh/simonsmh.github.io.git -b master && cd simonsmh.github.io && git pull origin master && chmod -R 777 * && chown -R www-data *','r');
+$handle = popen('cd '.$dir.' && git clone https://github.com/simonsmh/simonsmh.github.io.git -b master && cd simonsmh.github.io && git fetch && git reset --hard && git pull origin master && chmod -R 777 * && chown -R www-data *','r');
 $read = stream_get_contents($handle);
 printf($read);
 pclose($handle);
-}*/
+}
 elseif($_GET["url"] == 1){
 $http = "http://";
 $choose = $_POST['choose'];
